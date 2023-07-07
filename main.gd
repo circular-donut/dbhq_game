9117c50556c6fd66a23fe6d534945090c19598f5
circular_donut <jessie@daMacBook.local>
2023-07-07 15:23:22 -0500

    Music, background, foot sprite

--- main.gd
+++ main.gd
@@ -4,6 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$player.start($start_position.position)
+	$game_music.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
