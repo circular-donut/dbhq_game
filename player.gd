b3958f0143ae5cc041540af2bc1e37a0e020030a
circular_donut <jessie@daMacBook.local>
2023-07-07 19:12:35 -0500

    add rebounding input

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var angular_speed = PI
-@export var speed = 400 # How fast the player will move (pixels/sec).
+@export var speed = 600 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
 func _ready():
@@ -14,15 +14,29 @@ func start(pos):
 	$CollisionShape2D.disabled = false
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed(&"move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed(&"move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed(&"move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed(&"move_up"):
-		velocity.y -= 1
+	var velocity = Vector2.ZERO # The player's movement vector. \
+	#if any input
+	if  Input.is_action_pressed(&"move_right") || Input.is_action_pressed(&"move_left") || Input.is_action_pressed(&"move_down") ||Input.is_action_pressed(&"move_up"):
+		if Input.is_action_pressed(&"move_right"):
+			velocity.x += 1
+		if Input.is_action_pressed(&"move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed(&"move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed(&"move_up"):
+			velocity.y -= 1
+	#if no input return to center
+	else:
+		if position.x > 500:
+			velocity.x -= 1
+		if position.x < 480:
+			velocity.x += 1
+		if position.y > 500:
+			velocity.y -= 1
+		if position.y < 480:
+			velocity.y += 1
+		
+	
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -32,11 +46,3 @@ func _process(delta):
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-
-	# if velocity.x != 0:
-	# 	$AnimatedSprite2D.animation = &"right"
-	# 	$AnimatedSprite2D.flip_v = false
-	# 	$AnimatedSprite2D.flip_h = velocity.x < 0
-	# elif velocity.y != 0:
-	# 	$AnimatedSprite2D.animation = &"up"
-	# 	$AnimatedSprite2D.flip_v = velocity.y > 0
